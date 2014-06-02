be43364ecde0b1b1b4713349a50c51f69fc5fb32
Oscar Lima <olima_84@yahoo.com>
2014-06-02 22:51:22 +0200

    fixing tf orientation

--- microsoft_cam_driver/src/microsoft_tf_pub.cpp
+++ microsoft_cam_driver/src/microsoft_tf_pub.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
 		try
 		{
 			fake_tf.setOrigin( tf::Vector3(0.0, 0.0, 1.19) ); //shifting the origin
-			fake_tf.setRotation( tf::createQuaternionFromRPY(2.356194, 0.0, 0.0) ); //preserving the orientation of the parent
+			fake_tf.setRotation( tf::createQuaternionFromRPY(-2.356194, 0.0, 0.0) ); //preserving the orientation of the parent
 			tf_broadcaster.sendTransform(tf::StampedTransform(fake_tf, ros::Time::now(), "/world", "/microsoft_camera_frame"));
 		}
 		catch (tf::TransformException ex)
